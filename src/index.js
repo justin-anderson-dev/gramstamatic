8d9a2f17b3dc20e1e9c887a8428b02e7098209d5
Justin Anderson <justin@watchworddigital.com>
2021-08-30 14:28:17 -0700

    seed database and set up Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import FirebaseContext from './context/firebase';
+import { firebase, FieldValue } from './lib/firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
+
+// client side rendered app: react (cra)
+  // -> database (Firebase)
+  // -> react-loading-skeleton
+  // -> tailwind
+
+// src
+  // -> components
+  // -> constants
+  // -> context
+  // -> helpers
+  // -> hooks
+  // -> lib (firebase here)
+  // -> services (firebase functions here)
+  // -> styles (tailwind folder: app / tailwind)
---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyBI_c5UBou4OhfAaapSzIVFgUXMMzN9sQw',
+  authDomain: 'gramm-fans.firebaseapp.com',
+  projectId: 'gramm-fans',
+  storageBucket: 'gramm-fans.appspot.com',
+  messagingSenderId: '401933393341',
+  appId: '1:401933393341:web:0fa1c98f3def91cdd506d4',
+  measurementId: 'G-T9ZML7PN1W'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// call seed file - only once
+// seedDatabase(firebase);
+
+// console.log('firebase --> ', firebase);
+export { firebase, FieldValue };
