839e8308a8a019d2e5282f2a7622981db1bbd13f
Justin Anderson <justin@watchworddigital.com>
2021-08-30 23:17:40 -0700

    WIP login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -25,8 +25,33 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram profile" />
       </div>
       <div className="flex flex-col w-2/5">
-        <p>I will be the form.</p>
+        <h1 className="flex justify-center w-full">
+          <img src="/images/logo.png" alt="Instagram logo" className="mt-2 w-6/12 mb-4" />
+        </h1>
+
+        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+        <form onSubmit={handleLogin} method="POST">
+          <input
+            aria-label="Enter your email address"
+            type="text"
+            placeholder="Email address"
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <input
+            aria-label="Enter your password"
+            type="password"
+            placeholder="Password"
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </form>
       </div>
     </div>
   );
 }
+
+// text-red-primary -> hex value
+// text-gray-base -> hex value
+// border-gray-primary -> hex value
