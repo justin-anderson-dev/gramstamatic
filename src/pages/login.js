eee5849f85e51ff8f2179426b732f4a69a62cc3f
Justin Anderson <justin@watchworddigital.com>
2021-08-30 22:47:23 -0700

    install and config tailwind

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,32 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+import '../styles/app.css';
+
 export default function Login() {
-  return <p>I am the login page, sucker.</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Gramm.fans';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram profile" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>I will be the form.</p>
+      </div>
+    </div>
+  );
 }
