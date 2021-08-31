c07e6b156f64c0f6fff26721077c2a29c78a736d
Justin Anderson <justin@watchworddigital.com>
2021-08-31 11:30:38 -0700

    finish login page form layout

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,20 @@
 import { useState, useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import '../styles/app.css';
 
 export default function Login() {
+  // eslint-disable-next-line
   const history = useHistory();
+  // eslint-disable-next-line
   const { firebase } = useContext(FirebaseContext);
 
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
 
+  // eslint-disable-next-line
   const [error, setError] = useState('');
+  // eslint-disable-next-line
   const isInvalid = password === '' || emailAddress === '';
 
   const handleLogin = () => {};
@@ -25,33 +29,53 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram profile" />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img src="/images/logo.png" alt="Instagram logo" className="mt-2 w-6/12 mb-4" />
-        </h1>
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram logo" className="mt-2 w-6/12 mb-4" />
+          </h1>
 
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
-          <input
-            aria-label="Enter your password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </form>
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${
+                isInvalid && `opacity-50`
+              }`}
+            >
+              Log in
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+          <p className="text-sm">
+            Don't have an account?{` `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
 }
-
+// TODO: add to Tailwind config
+// bg-blue-medium -> hex value
 // text-red-primary -> hex value
 // text-gray-base -> hex value
 // border-gray-primary -> hex value
