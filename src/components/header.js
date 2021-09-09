4d23b72764efd1bb3b10d20c4714ca94a38089ec
Justin Anderson <justin@watchworddigital.com>
2021-09-09 12:17:31 -0700

    Add auth-listener hook

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,15 @@
+import { useContext } from "react";
+import { Link } from 'react-router-dom';
+import FirebaseContext from "../context/firebase";
+import UserContext from "../context/user";
+import * as ROUTES from '../constants/routes';
+
 export default function Header() {
+  const { firebase } = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+
+  console.log('user -> ', user);
+
   return (
     <p>I am the Header</p>
   );
---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,25 @@
+import { useState, useEffect, useContext } from "react";
+import FirebaseContext from "../context/firebase";
+
+export default function useAuthListener() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChanged((authUser) => {
+      // user is authenticated, store in localStorage and set app state 'user'
+      if (authUser) {
+        localStorage.setItem('authUser', JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        // no authenticated user, clear localStorage and app state 'user'
+        localStorage.removeItem('authUser');
+        setUser(null);
+      }
+    });
+
+    return () => listener();
+  }, [firebase]);
+
+  return { user };
+}
